3b0967cf2675ab952b46720688a27c94cf6e9776
ylagorebollar <ylagorebollar@atanasoff03.danielcastelao.org>
2016-06-06 09:24:04 +0200

    Cambio en visualizacion Comentarios de cada objeto creado

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -1,13 +1,25 @@
 package examenfinalparte1;
+
 import java.io.IOException;
+
+/**
+ *
+ * @author ylagorebollar
+ */
 public class Prin {
-public static void main(String[] args) throws IOException{
-Principal obj = new Principal(10,20);
-int n = obj.i;
-System.out.println("Es: " + n);
-Principal nm = new Principal(10);
-int m = nm.i;
-System.out.println("Es: " + m);
-Principal mn = new Principal();
-int nn = mn.i;
-System.out.println("Es: " + nn);}}
+
+    public static void main(String[] args) throws IOException {
+        
+        Principal sum1 = new Principal(10, 20); //Intancia un objeto de la clase Principal y le pasa dos parametros
+        int dosParametros = sum1.totalOperaciones;
+        System.out.println("El resultado para el metodo que recibe dos parametros es: " + dosParametros);
+
+        Principal sum2 = new Principal(10); //Intancia un objeto de la clase Principal y le pasa un parametros
+        int unParametro = sum2.totalOperaciones;
+        System.out.println("El resultado para el metodo que recibe un parametro es: " + unParametro);
+
+        Principal sum3 = new Principal(); //Intancia un objeto de la clase Principal sin pasarle parametros
+        int sinParametros = sum3.totalOperaciones;
+        System.out.println("El resultado del metodo que no recibe parametros es: " + sinParametros);
+    }
+}
